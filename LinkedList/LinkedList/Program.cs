eecf1aeb5fe5760242d3f28aac3de76c45cb4a3a
unknown <diptobiswas1998@gmail.com>
2020-10-26 08:20:36 +0530

    [Dipto] refactor: Creating a Linked List with three nodes

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -6,6 +6,11 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Linked List!");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
