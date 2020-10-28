a81cce602308f06f276074fba23cd90f1bca9825
unknown <diptobiswas1998@gmail.com>
2020-10-28 17:57:44 +0530

    [Dipto] refactor: Creating a Linked List with three nodes

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -5,27 +5,10 @@ namespace LinkedList
 {
     class LinkedList
     {
-        internal Node head;
-        //internal Node Tail;
-        internal void Add(int data)
+        //internal Node head;
+        internal void Display(Node head)
         {
-            Node node = new Node(data);
-            if (this.head == null)
-                this.head = node;
-            else
-            {
-                Node temp = head;
-                while (temp.next != null)
-                {
-                    temp = temp.next;
-                }
-                temp.next = node;
-            }
-            Console.WriteLine("{0} inserted into the linked list", node.data);
-        }
-        internal void Display()
-        {
-            Node temp = this.head;
+            Node temp = head;
             if (temp == null)
             {
                 Console.WriteLine("Linked list is empty");
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -7,10 +7,14 @@ namespace LinkedList
         {
             Console.WriteLine("Welcome to the Linked List!");
             LinkedList list = new LinkedList();
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
-            list.Display();
+            Node node1 = new Node(56);
+            Node node2 = new Node(30);
+            Node node3 = new Node(70);
+            node1.next = node2;
+            node2.next = node3;
+            node3.next = null;
+            Console.WriteLine("Linked List is displayed.");
+            list.Display(node1);
         }
     }
 }
