87b3b931d88dd5234ff976a770ee6d470e9e1c9b
unknown <diptobiswas1998@gmail.com>
2020-10-29 07:57:32 +0530

    [Dipto] refactor: Adding code to remove the first node

--- LinkedList/LinkedList/LinkedList.cs
+++ LinkedList/LinkedList/LinkedList.cs
@@ -80,5 +80,12 @@ namespace LinkedList
             }
             return head;
         }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+                return null;
+            this.head = this.head.next;
+            return this.head;
+        }
     }
 }
